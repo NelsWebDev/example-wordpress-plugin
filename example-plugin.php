25d9eb8cd523941029b62e2cd7b9ab08184669c3
TechTube <admin@techtubecentral.com>
2018-08-31 13:37:24 -1000

    Update example-plugin.php

--- example-plugin.php
+++ example-plugin.php
@@ -14,7 +14,7 @@ add_action("admin_menu", "addMenu");
 function addMenu()
 {
   add_menu_page("Example Options", "Example Options", 4, "example-options", "exampleMenu" );
-  add_submenu_page("example_options", "Option 1", "Option 1", 4, "example-option-1", "option1"
+  add_submenu_page("example_options", "Option 1", "Option 1", 4, "example-option-1", "option1");
 }
 
 function exampleMenu()
